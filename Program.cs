114938c5453e98912e4f8904d97b0ff1d3c6c774
sophiehauritz <sophiehauritz@outlook.com>
2025-09-01 16:12:03 +0200

    finAL PROJEKT

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿int Control(int altitude)
+{
+    int thruster = 0;
+    
+    // YOUR CODE HERE
+    if (altitude > 100)
+    {
+        thruster = 0; // Thruster OFF
+    }
+    else if (altitude > 0)
+    {
+        thruster = 1; // Thruster ON
+    }
+    else
+    {
+        thruster = 0; // Thruster OFF (landing or below)
+    }
+    
+    return thruster;
+}
+
+void Test(int altitude)
+{
+    int thruster = Control(altitude);
+    bool behaviorCorrect = (altitude > 100 && thruster == 0) ||
+                           (altitude is <= 100 and > 0 && thruster == 1) ||
+                           (altitude <= 0 && thruster == 0);
+    var behaviorCorrectIcon = behaviorCorrect ? "✅" : "❌";
+    Console.WriteLine($"For altitude {altitude}, your thruster is {thruster} |{behaviorCorrectIcon}|");
+}
+
+Test(150);
+Test(100);
+Test(50);
+Test(0);
+Test(-1);
